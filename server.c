2f759920bee3a54afcbe3e0ecfce6f4204b3bc4b
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-02-26 19:28:54 +0330

    socket & bind

--- server.c
+++ server.c
@@ -1,5 +1,7 @@
 #include "util.h"
 
+#define BACKLOG 10
+
 int  main(int argc, char const *argv[]) {
   if (argc != 2) {
     print("ERR! port number?");
@@ -14,8 +16,27 @@ int  main(int argc, char const *argv[]) {
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
   if (getaddrinfo(NULL, port, &hints, &res) != 0) {
-    print("ERR! getaddrinfo have error");
+    print("ERR! getaddrinfo");
     return 0;
   }
+
+  for(p = res; p != NULL; p = p->ai_next) {
+    if ((socketfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      print("ERR! socket");
+      continue;
+    }
+    if (setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      print("ERR! setsockopt");
+      return 0;
+    }
+    if (bind(socketfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(socketfd);
+      print("ERR! bind");
+      continue;
+    }
+    break;
+  }
+
+  
   return 0;
 }
